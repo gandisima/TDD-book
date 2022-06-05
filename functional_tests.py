07af51c9696c4fbdbf746d8d9f9f60380cdf2ac4
Jacek <jacek.rybczynski5@gmail.com>
2022-06-05 17:00:49 +0200

    Second item FT

--- functional_tests.py
+++ functional_tests.py
@@ -8,15 +8,15 @@ import unittest
 
 # Edith has heard about a cool new online to-do app. She goes
 # to check out its homepage
-class NewVisitorTest(unittest.TestCase):  
+class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self):  
+    def setUp(self):
         self.browser = webdriver.Chrome()
 
-    def tearDown(self):  
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
@@ -31,7 +31,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
-        )        
+        )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
@@ -46,17 +46,27 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-            )
+        self.assertIn(
+            '1: Buy peacock feathers', [row.text for row in rows],
+            f"New to-do item did not appear in table.\n Contents were: {table.text}"
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
-        self.fail('Finish the test!')  
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
@@ -65,6 +75,8 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
+        self.fail('Finish the test!')
 
-if __name__ == '__main__':  
+
+if __name__ == '__main__':
     unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,13 @@ from django.test import TestCase
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         # request = HttpRequest()
         # response = home_page(request)
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
