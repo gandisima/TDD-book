38a17820fea646d15e3fa73e6813f0c1dfe72369
Jacek <jacek.rybczynski5@gmail.com>
2022-06-08 14:52:31 +0200

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
 
 MAX_WAIT = 2
@@ -10,7 +10,7 @@ MAX_WAIT = 2
 # to check out its homepage
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -136,8 +136,10 @@ class NewVisitorTest(LiveServerTestCase):
         # centered there too
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1:testing')  
+        self.wait_for_row_in_list_table('1: testing')
+
         inputbox = self.browser.find_element_by_id('id_new_item')
+
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,6 @@
 from lists.models import Item, List
 from django.test import TestCase
 
-from lists.views import new_list
-
 
 class HomePageTest(TestCase):
 
@@ -40,12 +38,13 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
 
+
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertTemplateUsed(response, 'list.html') 
+        self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
@@ -61,7 +60,7 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
-    
+
     def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -70,7 +69,7 @@ class ListViewTest(TestCase):
 
 
 class NewListTest(TestCase):
-    
+
     def test_can_save_a_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
@@ -78,9 +77,11 @@ class NewListTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        response = self.client.post(
+            '/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
+
 
 class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
@@ -90,13 +91,12 @@ class NewItemTest(TestCase):
         self.client.post(
             f'/lists/{correct_list.id}/add_item',
             data={'item_text': 'A new item for an existing list'}
-            )
-        
+        )
+
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
-
 
     def test_redirects_to_list_view(self):
         other_list = List.objects.create()
@@ -105,6 +105,6 @@ class NewItemTest(TestCase):
         response = self.client.post(
             f'/lists/{correct_list.id}/add_item',
             data={'item_text': 'A new item for existing list'}
-            )
-        
+        )
+
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
